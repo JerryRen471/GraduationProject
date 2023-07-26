b29cfeb82604fa243e3004630143f180167a3158
Jerry Ren <jerrymay471@gmail.com>
2023-07-26 10:49:47 +0800

    file changes

--- 1toAll.sh
+++ 1toAll.sh
@@ -5,6 +5,18 @@ module load anaconda/2020.11
 module load cuda/11.3
 source activate myname
 
-python /data/home/scv7454/run/GraduationProject/TimeEvolution.py --pt_it 10 --time 100 --hl 0
+python /data/home/scv7454/run/GraduationProject/TimeEvolution.py --pt_it 10 --time 100 --hl 2
 python /data/home/scv7454/run/GraduationProject/ADQC.py --pt_it 10 --time 100
+python /data/home/scv7454/run/GraduationProject/TimeEvolution.py --pt_it 5 --time 100 --hl 2
+python /data/home/scv7454/run/GraduationProject/TimeEvolution.py --pt_it 15 --time 100 --hl 2
+python /data/home/scv7454/run/GraduationProject/TimeEvolution.py --pt_it 20 --time 100 --hl 2
+python /data/home/scv7454/run/GraduationProject/TimeEvolution.py --pt_it 25 --time 100 --hl 2
+python /data/home/scv7454/run/GraduationProject/TimeEvolution.py --pt_it 50 --time 100 --hl 2
+python /data/home/scv7454/run/GraduationProject/TimeEvolution.py --pt_it 100 --time 100 --hl 2
+python /data/home/scv7454/run/GraduationProject/1toAll.py --pt_it 5 --time 100
 python /data/home/scv7454/run/GraduationProject/1toAll.py --pt_it 10 --time 100
+python /data/home/scv7454/run/GraduationProject/1toAll.py --pt_it 15 --time 100
+python /data/home/scv7454/run/GraduationProject/1toAll.py --pt_it 20 --time 100
+python /data/home/scv7454/run/GraduationProject/1toAll.py --pt_it 25 --time 100
+python /data/home/scv7454/run/GraduationProject/1toAll.py --pt_it 50 --time 100
+python /data/home/scv7454/run/GraduationProject/1toAll.py --pt_it 100 --time 100
