e536950466ce0c6b8185feaca90e7f9e30ca9562
Jerry Ren <jerrymay471@gmail.com>
2023-09-05 11:33:05 +0800

    Print the final loss in log file

--- draw_rand.py
+++ draw_rand.py
@@ -9,12 +9,15 @@ parser.add_argument('--seed', type=int, default=100)
 parser.add_argument('--folder', type=str, default='rand_init/')
 parser.add_argument('--train_num', type=int, default=100)
 args = parser.parse_args()
+train_num = args.train_num
 
 results = np.load('GraduationProject/Data/'+args.folder+'adqc_result_num{:d}.npy'.format(args.train_num), allow_pickle=True) # results是字典, 包含'train_pred', 'test_pred', 'train_loss', 'test_loss'
 results = results.item()
 train_loss = results['train_loss']
 test_loss = results['test_loss']
 x = list(range(0, len(train_loss)))
+
+print('train_num={:d}\ntrain_loss:{:.4e}\ttest_loss:{:.4e}\n'.format(train_num, train_loss[-1], test_loss[-1]))
 
 legends = []
 plt.plot(x, train_loss, label='train loss')
