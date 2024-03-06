cfc88cf5b13a1afa73544d9c1ca0072ddf8d0d94
Jerry Ren <jerrymay471@gmail.com>
2024-03-06 16:09:13 +0800

    Test the diff between similarity & gate_fidelity

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
 # from cv2 import mean
-# import numpy as np
-# import torch as tc
+import numpy as np
+import torch as tc
 
-num_q = 10
-X_even = list(range(0, num_q, 2))
-X_odd = list(range(1, num_q-1, 2))
-X_poses = [X_even, X_odd]
-depth = 4
-for nd in range(depth):
-    for ng in X_poses[nd%2]:
-        print('ng+1',ng+1)
-        print('ng',ng)
+def gate_fidelity(E, U):
+    n = E.shape[0]
+    trace = tc.einsum('aa', U.T.conj() @ E)
+    gate_fidelity = 1/(n*(n+1))*(n + tc.abs(trace)**2)
+    return gate_fidelity
+
+def similarity(E:tc.Tensor, U:tc.Tensor):
+    '''
+    E: circuit
+    U: real process
+    '''
+    a = tc.norm(E - U)
+    b = 2 * tc.norm(U)
+    s = 1 - a/b
+    return s
+
+A = tc.rand([2,2], dtype=tc.complex128)
+u, s, v = tc.svd(A)
+U0 = tc.mm(u, v.T.conj())
+f = gate_fidelity(U0, (0.5+0.866j)*U0)
+s = similarity(U0, (0.5+0.866j)*U0)
+print('gate_fidelity',f)
+print('similarity', s)
