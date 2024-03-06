c703d0a7afdb4e639ba6a9617b19169be6945eaf
Jerry Ren <jerrymay471@gmail.com>
2024-03-06 15:32:17 +0800

    Test vqc structure

--- test.py
+++ test.py
@@ -1,52 +1,13 @@
 # from cv2 import mean
-import numpy as np
-import torch as tc
+# import numpy as np
+# import torch as tc
 
-# evol_mat = np.load('GraduationProject/Data/evol_mat.npy')
-# evol_mat = tc.from_numpy(evol_mat)
-# print('evol_mat.shape is', evol_mat.shape)
-
-# A = tc.mm(evol_mat, evol_mat.T.conj())
-# a = tc.diag(A)
-# print(a)
-
-'''用循环生成n个[1, m]的随机张量和直接生成一个[n, m]的随机张量是一样的'''
-tc.manual_seed(10)
-# print('for循环')
-# for _ in range(2):
-#     print(tc.rand([1,2]))
-print('tc.rand')
-print(tc.rand([2,2]))
-
-
-# tc.manual_seed(1)
-def rand_normal_dist_states(number:int, length:int, device=tc.device('cuda:0'))->tc.Tensor:
-    number = int(number)
-    shape = [1, 2 ** length]
-    states = tc.randn(shape, dtype=tc.complex128, device=device)
-    sigma = tc.rand([number,1], dtype=tc.complex128, device=device)*1
-    mu = tc.rand([number,1], dtype=tc.complex128, device=device)
-    print('mu=',mu)
-    states = states*sigma+mu
-    shape_ = [number] + [2]*length
-    norm = tc.sum(states * states.conj(), dim=1, keepdim=True)
-    print('norm=',norm)
-    # states = states / tc.sqrt(norm.real)
-    # states = states.reshape(shape_)
-    return states
-
-a = rand_normal_dist_states(2, 10, device=tc.device('cpu'))
-print('a=',a)
-norm = tc.sum(a * a.conj(), dim=1, keepdim=True)
-# print(norm)
-
-b = tc.sum(a, dim=[1])/a.shape[1]
-print(b)
-print(tc.var(a, dim=1))
-
-a = tc.tensor([[1, 2, 3], [4, 5, 6]])
-b = tc.tensor([[1],[2]])
-# print(a.shape)
-# print(b.shape)
-# print(a*b)
-
+num_q = 10
+X_even = list(range(0, num_q, 2))
+X_odd = list(range(1, num_q-1, 2))
+X_poses = [X_even, X_odd]
+depth = 4
+for nd in range(depth):
+    for ng in X_poses[nd%2]:
+        print('ng+1',ng+1)
+        print('ng',ng)
