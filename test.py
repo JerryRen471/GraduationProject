5793f704d3f1cd8a945f833522310e7491eb2b09
Jerry Ren <jerrymay471@gmail.com>
2024-03-11 20:05:09 +0800

    Test Haar random states random states generated by acting different Haar matrices to the same state

--- test.py
+++ test.py
@@ -2,26 +2,29 @@
 import numpy as np
 import torch as tc
 
-def gate_fidelity(E, U):
-    n = E.shape[0]
-    trace = tc.einsum('aa', U.T.conj() @ E)
-    gate_fidelity = 1/(n*(n+1))*(n + tc.abs(trace)**2)
-    return gate_fidelity
+def qr_Haar(number:int, length:int, device=tc.device('cuda:0'))->tc.Tensor:
+    number = int(number)
+    shape = [number, 2**length, 2**length]
+    real = tc.randn(shape, dtype=tc.float64, device=device)
+    imag = tc.randn(shape, dtype=tc.float64, device=device)
+    A = tc.complex(real, imag)
+    Q, R = tc.linalg.qr(A, 'complete')
+    Lambda = tc.diagonal(R, dim1=1, dim2=2)/tc.abs(tc.diagonal(R, dim1=1, dim2=2))
+    Haar_mat = tc.einsum('nij,nj->nij', Q, Lambda)
+    return Haar_mat
 
-def similarity(E:tc.Tensor, U:tc.Tensor):
-    '''
-    E: circuit
-    U: real process
-    '''
-    a = tc.norm(E - U)
-    b = 2 * tc.norm(U)
-    s = 1 - a/b
-    return s
+# print(qr_Haar(2, 2, device=tc.device('cpu')))
 
-A = tc.rand([2,2], dtype=tc.complex128)
-u, s, v = tc.svd(A)
-U0 = tc.mm(u, v.T.conj())
-f = gate_fidelity(U0, (0.5+0.866j)*U0)
-s = similarity(U0, (0.5+0.866j)*U0)
-print('gate_fidelity',f)
-print('similarity', s)
+def Haar_rand_states(number:int, length:int, device=tc.device('cuda:0'))->tc.Tensor:
+    number = int(number)
+    shape = [2 ** length]
+    state1 = tc.zeros(shape, dtype=tc.complex128, device=device)
+    state1[0] = 1
+    print(state1)
+    U = qr_Haar(number, length, device=device)
+    states = tc.einsum('nij,j->ni', U, state1)
+    return states
+
+states = Haar_rand_states(2,2, device=tc.device('cpu'))
+B = tc.einsum('ni,ni->n', states, states.conj())
+print(B)
