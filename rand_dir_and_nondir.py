eb15a1561b4a37f4a71469748117249861586963
Jerry Ren <jerrymay471@gmail.com>
2024-03-19 00:21:52 +0800

    Add function to generate Haar random states

--- rand_dir_and_nondir.py
+++ rand_dir_and_nondir.py
@@ -107,6 +107,29 @@ def rand_states_(number:int, length:int, device=tc.device('cuda:0'))->tc.Tensor:
     states = states.reshape(shape_)
     return states
 
+def qr_Haar(number:int, length:int, device=tc.device('cuda:0'))->tc.Tensor:
+    number = int(number)
+    shape = [number, 2**length, 2**length]
+    real = tc.randn(shape, dtype=tc.float64, device=device)
+    imag = tc.randn(shape, dtype=tc.float64, device=device)
+    A = tc.complex(real, imag)
+    Q, R = tc.linalg.qr(A, 'complete')
+    Lambda = tc.diagonal(R, dim1=1, dim2=2)/tc.abs(tc.diagonal(R, dim1=1, dim2=2))
+    Haar_mat = tc.einsum('nij,nj->nij', Q, Lambda)
+    return Haar_mat
+
+def Haar_rand_states(number:int, length:int, device=tc.device('cuda:0'))->tc.Tensor:
+    number = int(number)
+    shape = [2 ** length]
+    state1 = tc.zeros(shape, dtype=tc.complex128, device=device)
+    state1[0] = 1
+    print(state1)
+    U = qr_Haar(number, length, device=device)
+    states = tc.einsum('nij,j->ni', U, state1)
+    shape_ = [number] + [2]*length
+    states = states.reshape(shape_)
+    return states
+
 def rand_states(number:int, length:int, device=tc.device('cuda:0'))->tc.Tensor:
     number = int(number)
     shape = [number, 2 ** length]
@@ -134,8 +157,10 @@ def rand_dir_prod_states(number:int, length:int, device=tc.device('cuda:0'))->tc
 def gen_select(gen_type:str):
     if gen_type == 'd':
         gen = rand_dir_prod_states
-    else:
+    elif gen_type == 'n':
         gen = rand_states
+    elif gen_type == 'h':
+        gen = Haar_rand_states
     # else:
     #     gen = None
     #     print('-'*10+'\nArgument --gen_type must be the combination of \'n\' and \'d\'!\n'+'-'*10)
