9b245afe9a1e5b13aa32485fca069b906e414eb4
Jerry Ren <jerrymay471@gmail.com>
2024-05-31 09:57:15 +0800

    average entanglement entropy vs bond_entropy

--- avg_ee.py
+++ avg_ee.py
@@ -2,23 +2,24 @@ from matplotlib import pyplot as plt
 import torch as tc
 import numpy as np
 from rand_dir_and_nondir import rand_dir_prod_states, random_uni_evl, Haar_random_product_states
+from draw_bond import main
 
 def avg_ee_from_path(states, file_path):
     mat = np.load(file_path, allow_pickle=True)
-    print(mat.shape)
+    # print(mat.shape)
     mat = tc.from_numpy(mat)
     states_ = random_uni_evl(states, mat)
     shape = [num, int(2**(length/2)), int(2**(length/2))]
     states_ = states_.reshape(shape)
-    print(states_)
+    # print(states_)
     spectral = tc.linalg.svdvals(states_)
     p_distribution = spectral**2
-    print(p_distribution.shape)
+    # print(p_distribution.shape)
     entropy = tc.einsum("ij,ij->i", -p_distribution, tc.log2(p_distribution))
     avg_ee = tc.sum(entropy)/entropy.shape[0]
     return entropy, avg_ee
 
-num = 100
+num = 10000
 length = 10
 device = tc.device('cpu')
 states = Haar_random_product_states(num, length, device)
@@ -26,11 +27,21 @@ states = Haar_random_product_states(num, length, device)
 path_format = "/data/home/scv7454/run/GraduationProject/Data/evol_mat{}.npy"
 
 avg_ee_list = []
+e = []
 for i in range(10, 110, 10):
     path = path_format.format(i)
     ee, avg_ee = avg_ee_from_path(states, path)
     avg_ee_list.append(avg_ee)
-# print(ee)
+    bond, ee = main(path)
+    e.append(ee)
+print(avg_ee_list)
+print(e)
 plt.plot(avg_ee_list)
-plt.savefig("/data/home/scv7454/run/GraduationProject/pics/rand_states_entropy_diff_evol_time.svg")
+plt.savefig("/data/home/scv7454/run/GraduationProject/pics/10000rand_states_entropy_diff_evol_time.svg")
+plt.close()
+
+plt.plot(avg_ee_list, e)
+plt.xlabel('average_ee')
+plt.ylabel('bond_entropy')
+plt.savefig("/data/home/scv7454/run/GraduationProject/pics/10000avgVSbond.svg")
 plt.close()
--- draw_bond.py
+++ draw_bond.py
@@ -25,7 +25,7 @@ def half_bond(file_path):
 def entropy(lp):
     return tc.dot(-lp, tc.log2(lp))
 
-path_format = "/data/home/scv7454/run/GraduationProject/Data/rand_unitary/n{0}/{0}body_1e{1}noise_evol_mat.npy"
+# path_format = "/data/home/scv7454/run/GraduationProject/Data/rand_unitary/n{0}/{0}body_1e{1}noise_evol_mat.npy"
 
 # for i in [1,2,3,10]:
 #     plt.figure()
@@ -52,8 +52,7 @@ def cut_bond(bond, cut_value):
 # bond_cut = cut_bond(bond, 1)
 # print(bond_cut)
 
-file_path1 = "/data/home/scv7454/run/GraduationProject/Data/Heis/evol_mat.npy"
-file_path2 = "/data/home/scv7454/run/GraduationProject/Data/Heis/evol_mat100.npy"
+
 
 def main(file_path1):
     bond = half_bond(file_path1)
@@ -73,18 +72,22 @@ def main(file_path1):
 # plt.savefig("/data/home/scv7454/run/GraduationProject/pics/Heis/bond_diff_evol_time.svg")
 # plt.close()
 
-path_format = "/data/home/scv7454/run/GraduationProject/Data/evol_mat{}.npy"
-e = list()
-for i in range(10, 110, 10):
-    path = path_format.format(i)
-    bond, ee = main(path)
-    e.append(ee)
-print(e)
-plt.plot(e)
-plt.savefig("/data/home/scv7454/run/GraduationProject/pics/bond_diff_evol_time.svg")
-plt.close()
+if __name__ == '__main__':
+    file_path1 = "/data/home/scv7454/run/GraduationProject/Data/Heis/evol_mat.npy"
+    file_path2 = "/data/home/scv7454/run/GraduationProject/Data/Heis/evol_mat100.npy"
 
-gate_fidelity = [0.9998542,0.9987716,0.996651,0.9973619,0.9954584,0.9916964,0.9884063,0.9726967,0.9713727,0.9726504]
-ent = [0.3301,0.3837,0.5086,0.6147,0.7075,0.7905,0.8658,0.9353,1.0003,1.3142]
-plt.plot(ent, gate_fidelity)
-plt.savefig("/data/home/scv7454/run/GraduationProject/pics/fidelity_entropy.svg")
+    path_format = "/data/home/scv7454/run/GraduationProject/Data/evol_mat{}.npy"
+    e = list()
+    for i in range(10, 110, 10):
+        path = path_format.format(i)
+        bond, ee = main(path)
+        e.append(ee)
+    print(e)
+    plt.plot(e)
+    plt.savefig("/data/home/scv7454/run/GraduationProject/pics/bond_diff_evol_time.svg")
+    plt.close()
+
+    gate_fidelity = [0.9998542,0.9987716,0.996651,0.9973619,0.9954584,0.9916964,0.9884063,0.9726967,0.9713727,0.9726504]
+    ent = [0.3301,0.3837,0.5086,0.6147,0.7075,0.7905,0.8658,0.9353,1.0003,1.3142]
+    plt.plot(ent, gate_fidelity)
+    plt.savefig("/data/home/scv7454/run/GraduationProject/pics/fidelity_entropy.svg")
