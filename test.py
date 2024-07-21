a857c629235163ea9c2849a8486e02e13372ad6c
Jerry Ren <jerrymay471@gmail.com>
2024-07-21 22:14:49 +0800

    Test the use of torch.load() and torch.save()

--- test.py
+++ test.py
@@ -5,12 +5,8 @@ import time
 
 device=tc.device('cpu')
 dtype=tc.complex64
-length = 5
-a = TensorNetwork()
-a.add_node(tc.rand(2, 2))
-a.add_node(tc.rand(3, 3))
-a.add_node(tc.rand(3, 4))
-a.connect([1, 1], [2, 0])
-a.merge_nodes((0, 2))
-print(a.node_list[0].shape)
-print(a.connect_graph)
+mps = tc.load('test.pt')
+print(mps.center)
+print(mps.chi)
+print(len(mps.node_list))
+print(mps.connect_graph)
