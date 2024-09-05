1c94f72975c49cadd1e46391cecd3a5b371de027
Jerry Ren <jerrymay471@gmail.com>
2024-09-05 11:13:27 +0800

    Complete the code which uses TensorNetwork_pack to realize the time evolution

--- TN_rand_time_sequence.py
+++ TN_rand_time_sequence.py
@@ -1,14 +1,16 @@
-from cProfile import label
 import torch as tc
 import numpy as np
 from Library.BasicFun import mkdir
+from Library.TensorNetwork import TensorTrain_pack as tt_pack
+import Library.TensorNetwork as TN
 from Library import BasicFun as bf
 from Library import PhysModule as phy
 import random
+from copy import deepcopy
 
 from rand_dir_and_nondir import random_uni_evl
 
-def pure_states_evolution(states:tc.Tensor, gates:list, which_where:list)->tc.Tensor:
+def pure_states_evolution(states:tt_pack, gates:list, which_where:list):
     """Evolve the state by several gates0.
 
     :param state: initial state
@@ -17,24 +19,12 @@ def pure_states_evolution(states:tc.Tensor, gates:list, which_where:list)->tc.Te
     :return: evolved state
     Example: which_where = [[0, 1, 2], [1, 0, 1]] means gate 0 on spins
     1 and 2, and gate 1 on spins 0 and 1
-    """
-    def pure_states_evolution_one_gate(v, g, pos):
-        ind = list(range(len(pos), 2*len(pos)))
-        pos = [_+1 for _ in pos]
-        v = tc.tensordot(v, g, [pos, ind]) # 对每个pos加一
-        ind0 = list(range(v.ndimension()))
-        for nn in range(len(pos)):
-            ind0.remove(pos[nn])
-        ind0 += pos
-        order = np.argsort(ind0)
-        return v.permute(tuple(order))
-
+    """    
     for n in range(len(which_where)):
-        states = pure_states_evolution_one_gate(
-            states, gates[which_where[n][0]], which_where[n][1:])
+        states.act_two_body_gate(gates[which_where[n][0]], which_where[n][1:])
     return states
 
-def Heisenberg_mul_states_evl(states, para=None):
+def Heisenberg_mul_states_evl(states:tt_pack, para=None):
     para_def = dict()
     para_def['J'] = [0, 0, 1]
     para_def['h'] = [1.5, 0, 0]
@@ -52,6 +42,7 @@ def Heisenberg_mul_states_evl(states, para=None):
     para = dict(para_def, **para)
     para['d'] = phy.from_spin2phys_dim(para['spin'])
     para['time_it'] = round(para['time_tot'] / para['tau'])
+    para['print_time'] = para['print_time'] // para['tau']
     para['device'] = bf.choose_device(para['device'])
     # print(para)
 
@@ -90,12 +81,13 @@ def Heisenberg_mul_states_evl(states, para=None):
         which_where[0][0] = 1
         which_where[-1][0] = 2
     list_states = list()
+    states = deepcopy(states)
     for t in range(para['time_it']):
         states = pure_states_evolution(states, [gate, gate_l, gate_r], which_where)
         if t % para['print_time'] == para['print_time']-1:
-            list_states.append(states)
-    states = tc.stack(list_states, dim=1)
-    return states
+            list_states.append(deepcopy(states))
+    # states = tc.stack(list_states, dim=1)
+    return list_states
 
 '''用循环生成n个[1, m]的随机张量和直接生成一个[n, m]的随机张量是一样的'''
 def rand_states_(number:int, length:int, device=tc.device('cuda:0'))->tc.Tensor:
@@ -171,7 +163,7 @@ def rand_dir_prod_states(number:int, length:int, device=tc.device('cuda:0'))->tc
 
 def gen_select(gen_type:str):
     if gen_type == 'd':
-        gen = rand_dir_prod_states
+        gen = TN.rand_prod_mps_pack
     elif gen_type == 'n':
         gen = rand_states
     elif gen_type == 'h':
@@ -181,14 +173,17 @@ def gen_select(gen_type:str):
     #     print('-'*10+'\nArgument --gen_type must be the combination of \'n\' and \'d\'!\n'+'-'*10)
     return gen
 
-def label_generate(data, para):
-    label = Heisenberg_mul_states_evl(data, para)
-    input = tc.cat((data.unsqueeze(1), label[:, :-1]), dim=1)
-    merge = lambda x: x.reshape(x.shape[0]*x.shape[1], *x.shape[2:])
-    return merge(input), merge(label)
+def label_generate(data:tt_pack, para):
+    input = deepcopy(data)
+    label_list = Heisenberg_mul_states_evl(data, para)
+    label = label_list[0]
+    for i in range(len(label_list)-1):
+        input = TN.combine_mps_packs(input, label_list[i])
+        label = TN.combine_mps_packs(label, label_list[i+1])
+    return input, label
 
 if __name__ == '__main__':
-    length = 10
+    # length = 10
     spin = 'half'
     d = phy.from_spin2phys_dim(spin)
     device = tc.device('cuda:0')
@@ -196,38 +191,38 @@ if __name__ == '__main__':
 
     import argparse
     parser = argparse.ArgumentParser(description='manual to this script')
-    parser.add_argument('--n', type=int, default=3)
+    parser.add_argument('--length', type=int, default=10)
     parser.add_argument('--Jx', type=float, default=1)
-    parser.add_argument('--Jy', type=float, default=0)
+    parser.add_argument('--Jy', type=float, default=1)
     parser.add_argument('--Jz', type=float, default=1)
-    parser.add_argument('--hx', type=float, default=0.5)
-    parser.add_argument('--hl', type=float, default=2)
+    parser.add_argument('--hx', type=float, default=0)
+    parser.add_argument('--hl', type=float, default=0)
+    parser.add_argument('--time_interval', type=float, default=0.1)
+
+    parser.add_argument('--chi', type=int, default=10)
     parser.add_argument('--seed', type=int, default=None)
-    parser.add_argument('--train_num', type=int, default=100)
-    parser.add_argument('--test_num', type=int, default=500)
-    parser.add_argument('--folder', type=str, default='mixed_rand_states/')
+    parser.add_argument('--sample_num', type=int, default=1)
+    parser.add_argument('--evol_num', type=int, default=2)
+    parser.add_argument('--test_num', type=int, default=100)
+    parser.add_argument('--folder', type=str, default='GraduationProject/Data/TN_Heis/length12/loss_multi_mags/0.1/dn')
     parser.add_argument('--evol_mat_path', type=str, default="GraduationProject/Data/evol_mat.npy")
-    parser.add_argument('--gen_type', type=str, default='nn')
-    parser.add_argument('--time_tot', type=float, default=0.01)
+    parser.add_argument('--gen_type', type=str, default='dn')
     parser.add_argument('--tau', type=float, default=0.01)
     args = parser.parse_args()
 
     para = dict()
-    para['length'] = length
+    para['length'] = args.length
+    para['gen_type'] = args.gen_type
     para['spin'] = 'half'
     para['d'] = d
-    para['device'] = device
-    para['dtype'] = dtype
     para['J'] = [args.Jx, args.Jy, args.Jz]
     para['h'] = [args.hx, 0, 0]
-    para['print_time'] = args.time_tot*100
-    para['time_tot'] = args.time_tot*args.train_num
     para['hl'] = args.hl
-    para['folder'] = args.folder
+    para['chi'] = args.chi
+    para['device'] = device
+    para['dtype'] = dtype
     para['seed'] = args.seed
-    para['train_num'] = args.train_num
-    para['test_num'] = args.test_num
-    para['gen_type'] = args.gen_type
+    para['folder'] = args.folder
 
     if para['seed'] != None:
         tc.manual_seed(para['seed'])
@@ -235,32 +230,44 @@ if __name__ == '__main__':
     path = 'GraduationProject/Data'+para['folder']
     mkdir(path)
     para_evol = dict(para)
-    para_evol['time_tot'] = args.time_tot
+    para_evol['time_tot'] = args.time_interval
+    para_evol['print_time'] = args.time_interval
 
-    import os
-    evol_mat_path = args.evol_mat_path
-    if os.access(evol_mat_path, os.F_OK):
-        evol_mat = np.load(evol_mat_path, allow_pickle=True)
-        evol_mat = tc.from_numpy(evol_mat).cuda()
-    else:
-        E = tc.eye(2**para['length'], dtype=tc.complex128, device=para['device'])
-        shape_ = [E.shape[0]] + [2] * para['length']
-        E = E.reshape(shape_)
-        evol_mat = Heisenberg_mul_states_evl(E, para_evol).reshape(E.shape[0], -1)
-        print('evol_mat.shape is', evol_mat.shape)
-        np.save(evol_mat_path, evol_mat.cpu().T)
+    para_train = dict(para)
+    para_train['time_tot'] = args.time_interval*args.evol_num
+    para_train['print_time'] = args.time_interval
+    para_train['sample_num'] = args.sample_num
 
-    gen_train = gen_select(para['gen_type'][0])
-    trainset = gen_train(1, para['length'], device=para['device'])
-    trainset, train_lbs = label_generate(trainset, para)
+    para_test = dict(para)
+    para_test['time_tot'] = args.time_interval
+    para_test['print_time'] = args.time_interval
+    para_test['sample_num'] = args.sample_num
 
-    gen_test = gen_select(para['gen_type'][1])
-    testset = gen_train(1, para['length'], device=para['device'])
-    testset, test_lbs = label_generate(testset, para)
+    # import os
+    # evol_mat_path = args.evol_mat_path
+    # if os.access(evol_mat_path, os.F_OK):
+    #     evol_mat = np.load(evol_mat_path, allow_pickle=True)
+    #     evol_mat = tc.from_numpy(evol_mat).cuda()
+    # else:
+    #     ################# start from here #######################################
+    #     E = tc.eye(2**para['length'], dtype=tc.complex128, device=para['device'])
+    #     shape_ = [E.shape[0]] + [2] * para['length']
+    #     E = E.reshape(shape_)
+    #     evol_mat = Heisenberg_mul_states_evl(E, para_evol).reshape(E.shape[0], -1)
+    #     print('evol_mat.shape is', evol_mat.shape)
+    #     np.save(evol_mat_path, evol_mat.cpu().T)
+
+    # gen_train = gen_select(para['gen_type'][0])
+    trainset = TN.rand_prod_mps_pack(1, para['length'], chi=para['chi'], device=para['device'], dtype=para['dtype'])
+    trainset, train_lbs = label_generate(trainset, para_train)
+
+    # gen_test = gen_select(para['gen_type'][1])
+    testset = TN.rand_prod_mps_pack(1, para['length'], chi=para['chi'], device=para['device'], dtype=para['dtype'])
+    testset, test_lbs = label_generate(testset, para_test)
     data = dict()
     data['train_set'] = trainset
     data['train_lbs'] = train_lbs
     data['test_set'] = testset
     data['test_lbs'] = test_lbs
     
-    np.save(path+'/data_num{:d}'.format(args.train_num), data)
+    tc.save(data, path+'/train_set_sample_{:d}_evol_{:d}.pt'.format(args.sample_num, args.evol_num))
