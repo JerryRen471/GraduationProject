5fe87a429439709e9de9d6ad82d33ac18d5fef42
Jerry Ren <jerrymay471@gmail.com>
2024-09-05 11:19:31 +0800

    New feature: Load the model trained using less data as the initial model

--- rand_pred.py
+++ rand_pred.py
@@ -6,6 +6,29 @@ from Library import BasicFun as bf
 from Library import PhysModule as phy
 from Library.BasicFun import choose_device, mkdir
 from torch.utils.data import DataLoader, TensorDataset
+import os
+import re
+
+def search_qc(folder_path, evol_num):
+    # 使用正则表达式匹配文件名中的 evol 和 temp
+    pattern = re.compile(r'qc_param_sample_\d+_evol_(\d+)\.pt')
+
+    max_temp = None
+    max_temp_file = None
+
+    # 遍历文件夹中的所有文件
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.pt'):
+            match = pattern.search(filename)
+            if match:
+                temp = int(match.group(1))
+
+                # 检查 temp 小于给定的 evol_num，并且是最大的
+                if temp < evol_num and (max_temp is None or temp > max_temp):
+                    max_temp = temp
+                    max_temp_file = filename
+
+    return max_temp_file
 
 # 通用参数
 para = {'lr': 1e-2,  # 初始学习率
@@ -23,10 +46,10 @@ para_adqc = dict(para, **para_adqc)
 import argparse
 parser = argparse.ArgumentParser(description='manual to this script')
 # parser.add_argument('--seed', type=int, default=None)
-parser.add_argument('--folder', type=str, default="/loss_multi_mags/dn")
+parser.add_argument('--folder', type=str, default="/XXZ_inhomo/delta1/theta0/length10/loss_multi_mags/0.1/dn")
 parser.add_argument('--length', type=int, default=10)
 parser.add_argument('--sample_num', type=int, default=1)
-parser.add_argument('--evol_num', type=int, default=10)
+parser.add_argument('--evol_num', type=int, default=2)
 parser.add_argument('--loss_type', type=str, default='multi_mags')
 parser.add_argument('--depth', type=int, default=4)
 parser.add_argument('--rec_time', type=int, default=1)
@@ -47,9 +70,12 @@ data = data.item()
 print(data['train_set'].dtype)
 
 qc = ADQC.ADQC(para_adqc)
+old_qc_path = search_qc(folder_path=path, evol_num=args.evol_num)
+print(old_qc_path)
+if old_qc_path != None:
+    qc.load_state_dict(tc.load(path + '/' + old_qc_path), strict=False)
 qc, results_adqc, para_adqc = ADQC.train(qc, data, para_adqc)
 np.save(path+'/adqc_result_sample_{:d}_evol_{:d}'.format(args.sample_num, args.evol_num), results_adqc)
-
 
 qc.single_state = False
 E = tc.eye(2**para_adqc['length_in'], dtype=tc.complex128, device=para_adqc['device'])
@@ -62,8 +88,8 @@ with tc.no_grad():
 
 print('\nqc_mat.shape is', qc_mat.shape)
 np.save(path+'/qc_mat_sample_{:d}_evol_{:d}'.format(args.sample_num, args.evol_num), qc_mat.cpu())
+tc.save(qc.state_dict(), path+'/qc_param_sample_{:d}_evol_{:d}.pt'.format(args.sample_num, args.evol_num))
 
 # A = tc.mm(qc_mat, qc_mat.T.conj())
 # print(A)
 # print(tc.diag(A))
-
