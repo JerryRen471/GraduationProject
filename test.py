365263d079c78d7435202d186a34972a9251ca24
Jerry Ren <jerrymay471@gmail.com>
2024-09-05 11:21:16 +0800

    Test the function 'search_qc'

--- test.py
+++ test.py
@@ -1,25 +1,31 @@
-from copy import deepcopy
 import torch as tc
-from Library import PhysModule as phy
-from Library.TensorNetwork import *
-import time
+import re
+import os
+from matplotlib import pyplot as plt
+# from Library.TensorNetwork import TensorTrain_pack, copy_from_mps_pack
+# from Library.TN_ADQC import *
 
-device=tc.device('cpu')
-dtype=tc.complex64
-mps1 = rand_prod_mps_pack(4, 5, chi=4, phydim=2, device=device, dtype=dtype)
-mps2 = rand_prod_mps_pack(4, 5, chi=4, phydim=2, device=device, dtype=dtype)
-fidelity = inner_mps_pack(mps1, mps2)
-print(fidelity)
-sigma_z = tc.zeros([2, 2], device=device, dtype=dtype)
-sigma_z[0, 0] = 1+0.j
-sigma_z[1, 1] = -1+0.j
-print(sigma_z)
-mps_ = deepcopy(mps1)
-mps_.node_list = mps1.node_list[:]
-mps_.act_one_body_gate(sigma_z, 2)
-print(tc.dist(mps_.node_list[2], mps1.node_list[2]))
-hz = inner_mps_pack(mps1, mps_)
-hz_ = multi_mags_from_mps_pack(mps1, spins=[sigma_z])
-print(hz_.shape)
-print(hz_)
-# print(hz-hz_)
+def search_qc(folder_path, evol_num):
+    # 使用正则表达式匹配文件名中的 evol 和 temp
+    pattern = re.compile(r'qc_param_sample_\d+_evol_(\d+)\.pt')
+
+    max_temp = None
+    max_temp_file = None
+
+    # 遍历文件夹中的所有文件
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.pt'):
+            match = pattern.search(filename)
+            if match:
+                temp = int(match.group(1))
+
+                # 检查 temp 小于给定的 evol_num，并且是最大的
+                if temp < evol_num and (max_temp is None or temp > max_temp):
+                    max_temp = temp
+                    max_temp_file = filename
+
+    return max_temp_file
+
+path = 'GraduationProject/Data/PXP/length10/loss_multi_mags/0.7/d'
+old = search_qc(path, 7)
+print(old)
