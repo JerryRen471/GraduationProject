9f5907435af4f4598505f119dae8c58e1d981f96
Jerry Ren <jerrymay471@gmail.com>
2024-09-24 13:32:43 +0800

    1.在生成初态的函数中添加**kwargs关键字，兼容不同的参数 2.新增rand_entangled_states函数，可生成不同纠缠的随机态

--- rand_PXP.py
+++ rand_PXP.py
@@ -88,7 +88,7 @@ def fidelity_0_t(states):
         fidelity_list.append(fide_sqare)
     return tc.tensor(fidelity_list)
 
-def rand_states(number:int, length:int, device=tc.device('cuda:0'), dtype=tc.complex128)->tc.Tensor:
+def rand_states(number:int, length:int, device=tc.device('cuda:0'), dtype=tc.complex128, **kwargs)->tc.Tensor:
     number = int(number)
     shape = [number, 2 ** length]
     states = tc.randn(shape, dtype=dtype, device=device)
@@ -98,7 +98,7 @@ def rand_states(number:int, length:int, device=tc.device('cuda:0'), dtype=tc.com
     states = states.reshape(shape_)
     return states
 
-def rand_dir_prod_states(number:int, length:int, device=tc.device('cuda:0'), dtype=tc.complex128)->tc.Tensor:
+def rand_dir_prod_states(number:int, length:int, device=tc.device('cuda:0'), dtype=tc.complex128, **kwargs)->tc.Tensor:
     number = int(number)
     shape = [number, 2]
     states = tc.rand(shape, dtype=dtype, device=device)
@@ -112,7 +112,7 @@ def rand_dir_prod_states(number:int, length:int, device=tc.device('cuda:0'), dty
     states = states.reshape(shape_)
     return states
 
-def Z2_states(number:int, length:int, device=tc.device('cuda:0'), dtype=tc.complex64):
+def Z2_states(number:int, length:int, device=tc.device('cuda:0'), dtype=tc.complex64, **kwargs):
     state0 = tc.zeros([number, 2], device=device, dtype=dtype)
     state0[:, 0] = tc.ones([number], device=device, dtype=dtype)
     state1 = tc.zeros([number, 2], device=device, dtype=dtype)
@@ -128,13 +128,41 @@ def Z2_states(number:int, length:int, device=tc.device('cuda:0'), dtype=tc.compl
     states = states.reshape(shape_)
     return states
 
+def rq_decomposition(matrix):
+    # 对matrix的后两个维度进行转置
+    matrix_transposed = matrix.transpose(-2, -1)
+
+    q, r = tc.linalg.qr(matrix_transposed)
+    r = r.transpose(-2, -1)
+    q = q.transpose(-2, -1)
+    return r, q
+
+def rand_entangled_states(number:int, length:int, entangle_dim:int, device=tc.device('cuda:0'), dtype=tc.complex64):
+    L = length // 2
+    R = length - L
+    states_L = tc.randn([number, 2**L, entangle_dim], dtype=dtype, device=device)
+    states_L, _ = tc.linalg.qr(states_L)
+    states_R = tc.randn([number, entangle_dim, 2**R], dtype=dtype, device=device)
+    _, states_R = rq_decomposition(states_R)
+    states_mid = tc.eye(entangle_dim, dtype=dtype, device=device)
+    states = tc.einsum('ijk,kl,iln->ijn', states_L, states_mid, states_R)
+    states = states.reshape(number, -1)
+    print(states.shape)
+    shape_ = [number] + [2]*length
+    norm = tc.sum(states * states.conj(), dim=1, keepdim=True)
+    states = states / tc.sqrt(norm)
+    states = states.reshape(shape_)
+    return states
+
 def gen_select(gen_type:str):
-    if gen_type == 'd':
+    if gen_type == 'product' or gen_type == 'd':
         gen = rand_dir_prod_states
     elif gen_type == 'n':
         gen = rand_states
     elif gen_type == 'Z2':
         gen = Z2_states
+    elif gen_type == 'entangled':
+        gen = rand_entangled_states
     # else:
     #     gen = None
     #     print('-'*10+'\nArgument --gen_type must be the combination of \'n\' and \'d\'!\n'+'-'*10)
@@ -160,6 +188,7 @@ if __name__ == '__main__':
     parser.add_argument('--sample_num', type=int, default=1)
     parser.add_argument('--evol_num', type=int, default=10)
     parser.add_argument('--test_num', type=int, default=500)
+    parser.add_argument('--entangle_dim', type=int, default=1)
     parser.add_argument('--folder', type=str, default='mixed_rand_states/')
     parser.add_argument('--evol_mat_path', type=str, default="GraduationProject/Data/evol_mat.npy")
     parser.add_argument('--gen_type', type=str, default='Z2')
@@ -208,11 +237,11 @@ if __name__ == '__main__':
     np.save(evol_mat_path, evol_mat.cpu().T)
 
     gen_train = gen_select(para['gen_type'])
-    trainset = gen_train(para_train['sample_num'], para['length'], device=para['device'], dtype=para['dtype'])
+    trainset = gen_train(para_train['sample_num'], para['length'], device=para['device'], dtype=para['dtype'], entangle_dim=args.entangle_dim)
     trainset, train_lbs = label_generate(trainset, para_train)
 
     gen_test = gen_select('d')
-    testset = gen_train(para_test['sample_num'], para['length'], device=para['device'], dtype=para['dtype'])
+    testset = gen_test(para_test['sample_num'], para['length'], device=para['device'], dtype=para['dtype'], entangle_dim=args.entangle_dim)
     testset, test_lbs = label_generate(testset, para_test)
     data = dict()
     data['train_set'] = trainset
