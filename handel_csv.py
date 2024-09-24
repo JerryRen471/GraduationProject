1507b0064aa2ece52e33028146b0ca57fcd93207
Jerry Ren <jerrymay471@gmail.com>
2024-09-24 16:53:48 +0800

    debug：dataframe的拼接用pd.concat而不是append

--- handel_csv.py
+++ handel_csv.py
@@ -5,7 +5,7 @@ df_old = pd.read_csv("/data/home/scv7454/run/GraduationProject/Data/PXP.csv")
 df_new = pd.read_csv("/data/home/scv7454/run/GraduationProject/Data/PXP_tmp.csv")
 
 # Append the new rows to the end of the old dataframe
-df = df_old.append(df_new, ignore_index=True)
+df = pd.concat([df_old, df_new], ignore_index=True)
     
 # Write the updated dataframe back to the CSV file
 df.to_csv("/data/home/scv7454/run/GraduationProject/Data/PXP.csv", index=False)
